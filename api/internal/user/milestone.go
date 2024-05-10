00caf10a152197d64a3f5285aa031266261367f5
Kadeem Austin <dev@kadeemaustin.ai>
2024-05-09 20:31:22 -0400

    feat(api): add bson mappings for fields in Milestone struct

--- api/internal/user/milestone.go
+++ api/internal/user/milestone.go
@@ -2,11 +2,12 @@ package user
 
 import "time"
 
+// Milestone represents an identified step on the way to achieving a goal.
 type Milestone struct {
-	Title          string    `json:"title"`
-	Description    string    `json:"description"`
-	TargetDate     time.Time `json:"target_date,omitempty"`
-	IsComplete     bool      `json:"is_complete"`
-	IsDeleted      bool      `json:"is_deleted,omitempty"`
-	CompletionDate time.Time `json:"completion_date,omitempty"`
+	Title          string    `json:"title" bson:"title"`
+	Description    string    `json:"description" bson:"description"`
+	TargetDate     time.Time `json:"target_date,omitempty" bson:"target_date,omitempty"`
+	IsComplete     bool      `json:"is_complete" bson:"is_complete"`
+	CompletionDate time.Time `json:"completion_date,omitempty" bson:"completion_date,omitempty"`
+	IsDeleted      bool      `json:"is_deleted" bson:"is_deleted"`
 }
